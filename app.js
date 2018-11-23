2149f9fa9070879f07ac044c1ea1b129546ba77e
paulchin <paulchin20@yahoo.com>
2018-11-23 16:05:48 +0800

    deleteall

--- app.js
+++ app.js
@@ -85,6 +85,16 @@ app.get('/count', (req, res) => {
     });
 });
 
+app.get('/deleteall', (req, res) => {
+  Sensor.deleteMany({})
+  .then((response)=>{
+    res.status(200).send(response);
+  })
+  .catch((error)=>{
+    res.status(400).send(error);
+  })
+});
+
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
